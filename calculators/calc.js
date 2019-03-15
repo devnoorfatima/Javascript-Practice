2cab8d9a51303bc14987b5ffaef09b9a714fe7ba
Noor Fatima <devnoorfatima@gmail.com>
2019-03-15 16:13:41 +0500

    Attached the keypad with calculator

--- calculators/calc.js
+++ calculators/calc.js
@@ -1,111 +1,155 @@
-const calculator = {
-  displayValue: '0',
-  firstOperand: null,
-  waitingForSecondOperand: false,
-  operator: null,
-};
 
-// putting digits in display
-function inputDigit(digit) {
-  const { displayValue, waitingForSecondOperand } = calculator;
+// Select all the from document using queryselectAll
+var keys = document.querySelectorAll('#calculator span');
+// Define operators
+var operators = ['+', '-', 'x', '÷', '^'];
+// Set decimal flag for use later
+var decimalAdded = false;
 
-  if (waitingForSecondOperand === true) {
-    calculator.displayValue = digit;
-    calculator.waitingForSecondOperand = false;
-  } else {
-    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
-  }
+// loop through all keys
+for(var i = 0; i < keys.length; i++) {
+  //add onclick event to the keys
+	keys[i].onclick = function(e) {
+		// Get the input and button values
+		var input = document.querySelector('.screen');
+		var inputVal = input.innerHTML;
+		var btnVal = this.innerHTML;
+    
+		// If clear key is pressed, erase everything
+		if(btnVal == 'C') {
+			input.innerHTML = '';
+			decimalAdded = false;
+		}
+		
+		// If eval key is pressed, calculate and display the result
+		else if(btnVal == '=') {
+			var equation = inputVal; //append equation to variable
+			var lastChar = equation[equation.length - 1]; //target the end of the eval string
+     	
+			// Use regex to replace all instances of x, ÷, ^ with *, / and **
+			equation = equation.replace(/x/g, '*').replace(/÷/g, '/').replace(/\^/g, '\*\*');
+			
+			//Use regex to remove decimals from the end of an equation
+			if(operators.indexOf(lastChar) > -1 || lastChar == '.')
+				equation = equation.replace(/.$/, '');
+      
+			// use javascript's eval function to get the result
+      
+			if(equation)
+				input.innerHTML = eval(equation);	
+			  decimalAdded = false;
+		}
+		
+		// Javascript checks
+
+    // No two operators should be added consecutively.		
+		else if(operators.indexOf(btnVal) > -1) {
+			// Get the last character from the equation
+			var lastChar = inputVal[inputVal.length - 1];
+			
+			// Only add operator if input is not empty 
+			if(inputVal != '' && operators.indexOf(lastChar) == -1) 
+				input.innerHTML += btnVal;
+			
+			// Allow minus operator if the string is empty
+			else if(inputVal == '' && btnVal == '-') 
+				input.innerHTML += btnVal;
+			
+			// Replace the last operator (if exists) with the newly pressed operator
+			if(operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
+				input.innerHTML = inputVal.replace(/.$/, btnVal);
+			}
+			
+			decimalAdded =false;
+		}
+    // allow decimal point input
+		else if(btnVal == '.') {
+			if(!decimalAdded) {
+				input.innerHTML += btnVal;
+				decimalAdded = true;
+			}
+		}
+		
+		// if any other key is pressed, just append it after the decimal
+		else {
+			input.innerHTML += btnVal;
+		}
+		
+		// prevent page jumps
+		e.preventDefault();
+	} 
 }
-// putting decimal point in display
-function inputDecimal(dot) {
-  if (calculator.waitingForSecondOperand === true) return;
 
-  // If the `displayValue` does not contain a decimal point
-  if (!calculator.displayValue.includes(dot)) {
-    // Append the decimal point
-    calculator.displayValue += dot;
-  }
-}
-// putting the operators in display
-function handleOperator(nextOperator) {
-  const { firstOperand, displayValue, operator } = calculator
-  const inputValue = parseFloat(displayValue);
+//adding keyboard input functionality
+document.onkeydown = function(event) {
 
-  if (operator && calculator.waitingForSecondOperand)  {
-    calculator.operator = nextOperator;
- 
-    return;
-  }
+	var key_press = String.fromCharCode(event.keyCode);
+	var key_code = event.keyCode;
+	var input = document.querySelector('.screen');
+	var inputVal = input.innerHTML;
+	var btnVal = this.innerHTML;
+  var lastChar = inputVal[inputVal.length - 1];
+  var equation = inputVal;
+	// Using regex to replace all instances of x, ÷, ^ with *, / and ** respectively. 
+	equation = equation.replace(/x/g, '*').replace(/÷/g, '/').replace(/\^/g, '**');
 
-  if (firstOperand == null) {
-    calculator.firstOperand = inputValue;
-  } else if (operator) {
-    const currentValue = firstOperand || 0;
-    const result = performCalculation[operator](currentValue, inputValue);
-
-    calculator.displayValue = String(result);
-    calculator.firstOperand = result;
-  }
-
-  calculator.waitingForSecondOperand = true;
-  calculator.operator = nextOperator;
+ // Target each keypress and update the input screen
   
+    if(key_press==1) {
+      input.innerHTML += key_press;
+  }
+    if(key_press==2) {
+      input.innerHTML += key_press; 
+  }
+    if(key_press==3 || key_code == 32) {
+      input.innerHTML += key_press; 
+  }
+    if(key_press==4) {
+      input.innerHTML += key_press; 
+  }
+    if(key_press==5) {
+      input.innerHTML += key_press; 
+  }
+    if(key_press==6 && event.shiftKey == false) {
+      input.innerHTML += key_press; 
+  }
+    if(key_press==7) {
+      input.innerHTML += key_press; 
+  }
+    if(key_press==8 && event.shiftKey == false) {
+      input.innerHTML += key_press; 
+  }
+    if(key_press==9) {
+      input.innerHTML += key_press; 
+  }
+    if(key_press==0) {
+      input.innerHTML += key_press;
+  }
+  
+  // Cature operators and prevent from addint two consecutuve operators
+  
+    if ((inputVal != '' && operators.indexOf(lastChar) == -1 && key_code == 187 && event.shiftKey) || (key_code == 107) || (key_code == 61 && event.shiftKey)) {
+      document.querySelector('.screen').innerHTML += '+';
+  }
+    if ((inputVal != '' && operators.indexOf(lastChar) == -1 && key_code == 189 && event.shiftKey) || (inputVal != '' && operators.indexOf(lastChar) == -1 && key_code == 107)) {
+      document.querySelector('.screen').innerHTML += '-';
+  }
+    if ((inputVal != '' && operators.indexOf(lastChar) == -1 && key_code == 56 && event.shiftKey) || (inputVal != '' && operators.indexOf(lastChar) == -1 && key_code == 106)) {
+      document.querySelector('.screen').innerHTML += 'x';
+  }
+    if ((inputVal != '' && operators.indexOf(lastChar) == -1 && key_code == 191) || (inputVal != '' && operators.indexOf(lastChar) == -1 && key_code == 111)) {
+      document.querySelector('.screen').innerHTML += '÷';
+  }
+    if ((inputVal != '' && operators.indexOf(lastChar) == -1 && key_code == 54 && event.shiftKey)) {
+      document.querySelector('.screen').innerHTML += '^';
+  }
+    if(key_code==13 || key_code==187 && event.shiftKey == false) {
+      input.innerHTML = eval(equation);
+      //reset decimal added flag
+      decimalAdded =false;
+  }
+    if(key_code==8 || key_code==46) {
+			input.innerHTML = '';
+			decimalAdded = false;
+  }
 }
-
-// new object
-const performCalculation = {
-  '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
-
-  '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
-
-  '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
-
-  '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
-
-  '=': (firstOperand, secondOperand) => secondOperand
-};
-
-// reseting the calculator
-function resetCalculator() {
-  calculator.displayValue = '0';
-  calculator.firstOperand = null;
-  calculator.waitingForSecondOperand = false;
-  calculator.operator = null;
- 
-}
-//updating the dislplay
-function updateDisplay() {
-  const display = document.querySelector('.calculator-screen');
-  display.value = calculator.displayValue;
-}
-
-updateDisplay();
-
-const keys = document.querySelector('.calculator-keys');
-keys.addEventListener('click', (event) => {
-  const  target = event.target;
-  if (!target.matches('button')) {
-    return;
-  }
-
-  if (target.classList.contains('operator')) {
-      handleOperator(target.value);
-      updateDisplay();
- 
-    return;
-  }
-
-  if (target.classList.contains('decimal')) {
-      inputDecimal(target.value);
-      updateDisplay();
-    return;
-  }
-
-  if (target.classList.contains('all-clear')) {
-      resetCalculator();
-      updateDisplay();
-    return;
-  }
-  inputDigit(target.value);
-  updateDisplay();
-});
