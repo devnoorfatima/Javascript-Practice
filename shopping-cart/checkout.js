e2eb2f593b78028d806fe697f2b102afe1cee74b
Noor Fatima <devnoorfatima@gmail.com>
2019-04-06 00:43:08 +0500

    Making checkout page for shopping cart project

--- shopping-cart/checkout.js
+++ shopping-cart/checkout.js
@@ -5,25 +5,35 @@ let input = document.querySelector(".caps");
 let text = document.getElementById("text");
 
 // When the user presses any key on the keyboard, run the function
-input.addEventListener("keyup", function(event) {
+input.addEventListener("keyup", function (event) {
 
-  // If "caps lock" is pressed, display the warning text
-  if (event.getModifierState("CapsLock")) {
-    text.style.display = "block";
-  } else {
-    text.style.display = "none"
-  }
+    // If "caps lock" is pressed, display the warning text
+    if (event.getModifierState("CapsLock")) {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none"
+    }
 });
 
-function display() {
-    const form=document.querySelector('.form');
-    const icons=document.querySelector('.icons');
-    const header=document.querySelector('.head');
-    const final=document.querySelector('.final');
-   if(input.value){
-    form.style.display= 'none';
-   icons.style.display= 'none';
-   header.style.display= 'none';
-   final.style.display= 'none';
-}
-}
+function phonenumber(inputtxt) {
+    let phoneno = /^\d{11}$/;
+    if ((inputtxt.value.match(phoneno))) {
+            return true;
+        } else {
+            alert("message");
+            return false;
+        }
+    }
+
+    // function display() {
+    //     const form=document.querySelector('.form');
+    //     const icons=document.querySelector('.icons');
+    //     const header=document.querySelector('.head');
+    //     const final=document.querySelector('.final');
+    //     if(input.value){
+    //     form.style.display= 'none';
+    //    icons.style.display= 'none';
+    //    header.style.display= 'none';
+    //    final.style.display= 'none';
+    // }
+    // }
