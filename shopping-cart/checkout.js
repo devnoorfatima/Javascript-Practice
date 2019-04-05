ce9e3c6a39626a60f708d561d68e926d7459da37
Noor Fatima <devnoorfatima@gmail.com>
2019-04-05 23:46:31 +0500

    Making checkout page for shopping cart project

---
+++ shopping-cart/checkout.js
@@ -0,0 +1,29 @@
+// Get the input field
+let input = document.querySelector(".caps");
+
+// Get the warning text
+let text = document.getElementById("text");
+
+// When the user presses any key on the keyboard, run the function
+input.addEventListener("keyup", function(event) {
+
+  // If "caps lock" is pressed, display the warning text
+  if (event.getModifierState("CapsLock")) {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none"
+  }
+});
+
+function display() {
+    const form=document.querySelector('.form');
+    const icons=document.querySelector('.icons');
+    const header=document.querySelector('.head');
+    const final=document.querySelector('.final');
+   if(input.value){
+    form.style.display= 'none';
+   icons.style.display= 'none';
+   header.style.display= 'none';
+   final.style.display= 'none';
+}
+}
