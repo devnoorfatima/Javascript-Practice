2ead5c4534263f2c40eb2f23443a707f137c6898
Noor Fatima <devnoorfatima@gmail.com>
2019-04-10 20:38:14 +0500

    Used JSON method to get api and used it to make local weather app.

--- weather-app/script.js
+++ weather-app/script.js
@@ -6,17 +6,44 @@ $.getJSON('https://geoip-db.com/json/')
   $('#latitude').html(location.latitude);
   $('#longitude').html(location.longitude);
   $('#ip').html(location.IPv4);
-  var city = [location.city];
    var lon = [location.longitude];
    var lat = [location.latitude];
   $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon)
   .done(function(data){
     $('#temperature').html(data.main.temp.toFixed(1)+ "°C");
     $('#type').html(`${data.weather[0].main}`);
+    
+    if(data.main.temp<=1){
+     $('body').css("background","url(./snow.jpeg) fixed no-repeat");
+     $('body').css("background-size","cover");
+     $('body').css("color","Black");
+     $('.cel').css("color","Black");
+     $('.far').css("color","Black");
+    }
+    else if(data.main.temp<=20){
+    $('body').css("background","url(./cold.jpeg) fixed no-repeat");
+     $('body').css("background-size","cover");
+     $('body').css("color","Black");
+     $('.cel').css("color","Black");
+     $('.far').css("color","Black");
+    }
+    else if(data.main.temp<=35){
+        $('body').css("background","url(./mild.jpeg) fixed no-repeat");
+        $('body').css("background-size","cover");
+        $('body').css("color","Black");
+        $('.cel').css("color","Black");
+        $('.far').css("color","Black");
+    }
+    else if(data.main.temp>35){
+        $('body').css("background","url(./sunny.jpeg) fixed no-repeat");
+        $('body').css("background-size","cover");
+        $('body').css("color","Black");
+        $('.cel').css("color","Black");
+        $('.far').css("color","Black");
+    }
 })
 });
 function getFarenheit(){
-    // let farenheit = (parseInt(data.main.temp) * 1.8) + 32
     $("#temperature").html(((parseInt($("#temperature").text()) * 1.8) + 32).toFixed(1)+ '°F');
     $('.cel').css("opacity", "0.5");
     $('.far').css("opacity", "1");
@@ -25,10 +52,9 @@ function getFarenheit(){
     $(".cel").prop("disabled", false);
 };
 function getCelcius(){
-    $("#temperature").html(Math.round((parseInt($("#temperature").text()) - 32)/ 1.8).toFixed(1));
+    $("#temperature").html(Math.round((parseInt($("#temperature").text()) - 32)/ 1.8).toFixed(1) + '°C' );
     $('.far').css("opacity", "0.5");
     $('.cel').css("opacity", "1");
     $(".cel").prop("disabled", true);
     $(".far").prop("disabled", false);
 }
-
