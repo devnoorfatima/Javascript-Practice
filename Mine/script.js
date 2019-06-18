47a165dc2866b4259bcbcf89058bd817dda99b95
Noor Fatima <devnoorfatima@gmail.com>
2019-06-19 01:06:15 +0500

    Making a portfoliportfolio section.

---
+++ Mine/script.js
@@ -0,0 +1,7 @@
+const portfolio = document.querySelector('#portfolio');
+const imgSrc = ['./shots/1.png ', './shots/2.png', './shots/3.png', './shots/4.png', './shots/5.png',
+    './shots/6.png', './shots/7.png', './shots/8.png', './shots/9.png', './shots/10.png', './shots/11.png', './shots/12.png'
+]
+const name = ['Shopping Cart', 'Calculators', 'Weather App', 'Javascript Quiz', 'Ajax Type Ahead',
+    'Todo List', 'Random Quote Machine', 'Tomato Timer', 'HTML5 Canvas', 'Javascript Image Gallery', 'Indecision App', 'CSS Clock'
+]
