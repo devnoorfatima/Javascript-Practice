c408abd2bc3a4c61622dbb9d905fe060b9a930af
Noor Fatima <devnoorfatima@gmail.com>
2019-06-19 20:52:50 +0500

    Making a portfolio section.

--- Mine/script.js
+++ Mine/script.js
@@ -1,7 +1,21 @@
-const portfolio = document.querySelector('#portfolio');
 const imgSrc = ['./shots/1.png ', './shots/2.png', './shots/3.png', './shots/4.png', './shots/5.png',
     './shots/6.png', './shots/7.png', './shots/8.png', './shots/9.png', './shots/10.png', './shots/11.png', './shots/12.png'
 ]
 const name = ['Shopping Cart', 'Calculators', 'Weather App', 'Javascript Quiz', 'Ajax Type Ahead',
     'Todo List', 'Random Quote Machine', 'Tomato Timer', 'HTML5 Canvas', 'Javascript Image Gallery', 'Indecision App', 'CSS Clock'
 ]
+
+function addToPortfolio() {
+    const portfolio = document.querySelector('#portfolio');
+    const html = [];
+    for (let i = 0; i < name.length; i++) {
+        html.push(`
+        <div class="col-md-4 portfolio-item">
+            <img class="img-responsive shot" src="${imgSrc[i]}">
+            <h4>${name[i]}</h4>
+        </div>
+        `)
+    }
+    portfolio.innerHTML = html.join('');
+}
+window.addEventListener('DOMContentLoaded',addToPortfolio);
