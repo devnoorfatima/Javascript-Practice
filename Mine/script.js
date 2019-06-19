7a36d56aef4ca4ea783528b2fef625e9801cdde8
Noor Fatima <devnoorfatima@gmail.com>
2019-06-19 23:02:39 +0500

    Making a portfolio section.

--- Mine/script.js
+++ Mine/script.js
@@ -4,18 +4,31 @@ const imgSrc = ['./shots/1.png ', './shots/2.png', './shots/3.png', './shots/4.p
 const name = ['Shopping Cart', 'Calculators', 'Weather App', 'Javascript Quiz', 'Ajax Type Ahead',
     'Todo List', 'Random Quote Machine', 'Tomato Timer', 'HTML5 Canvas', 'Javascript Image Gallery', 'Indecision App', 'CSS Clock'
 ]
+const link = ['https://devnoorfatima.github.io/Projects/shopping-cart/', 'https://devnoorfatima.github.io/Projects/calculators/', 'https://devnoorfatima.github.io/Projects/weather-app/', 'https://devnoorfatima.github.io/Projects/Javascript-Quiz/', 'https://devnoorfatima.github.io/Projects/Ajax-type-ahead/',
+    'https://devnoorfatima.github.io/Projects/todo-list/', 'https://devnoorfatima.github.io/Projects/quote-machine/', 'https://devnoorfatima.github.io/Projects/tomato-timer/', 'https://devnoorfatima.github.io/Projects/HTML-5-Canvas/', 'https://devnoorfatima.github.io/Projects/Javascript-image-gallery/', 'https://github.com/devnoorfatima/React/tree/master/indecision-app', 'https://devnoorfatima.github.io/Projects/CSS-Clock/'
+]
+const code = ['']
 
 function addToPortfolio() {
     const portfolio = document.querySelector('#portfolio');
     const html = [];
     for (let i = 0; i < name.length; i++) {
         html.push(`
-        <div class="col-md-4 portfolio-item">
+        <div class="col-md-4 my-3 portfolio-item">
             <img class="img-responsive shot" src="${imgSrc[i]}">
             <h4>${name[i]}</h4>
+       
+    <div class="row mr-0 text-center ">
+    <div class="col-sm-6">
+    <a class='btn' href='${link[i]}' target='_blank' >Live Link</a>
+    </div>
+    <div class="col-sm-6">
+    <a class='btn' href='${link[i]}' target='_blank' >Code</a>
+    </div>
+    </div>
         </div>
         `)
     }
     portfolio.innerHTML = html.join('');
 }
-window.addEventListener('DOMContentLoaded',addToPortfolio);
+window.addEventListener('DOMContentLoaded', addToPortfolio);
